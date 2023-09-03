4bf49bd75e1b13684f9c4dc4d6bd114f6802616b
VovaJun123 <121401616+VovaJun123@users.noreply.github.com>
2023-09-03 16:57:11 +0100

    commit new

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,20 +1,88 @@
 
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
+// import Notiflix from "notiflix";
+// const input = document.querySelector('#datetime-picker');
+// const startBtn = document.querySelector('button[data-start]');
+
+// let timerDays = document.querySelector('span[data-days]');
+// let timerHours = document.querySelector('span[data-hours]');
+// let timerMinutes = document.querySelector('span[data-minutes]');
+// let timerSeconds = document.querySelector('span[data-seconds]');
+
+// const currentDate = new Date();
+
+// startBtn.disabled = true;
+// let timerId;
+
+// const fp = flatpickr(input, {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     if (selectedDates[0].getTime() - currentDate.getTime() < 0) {
+//       Notiflix.Report.warning(
+//         'WARNING!',
+//         'Please choose a date in the feature',
+//         'Ok'
+//       );
+//     } else {
+//       startBtn.disabled = false;
+//       startBtn.addEventListener('click', () => {
+//         const timerId = setInterval(() => {
+//           const currentTime = new Date();
+//           const ms = selectedDates[0].getTime() - currentTime.getTime();
+
+//           timerDays.textContent = convertMs(ms).days;
+//           timerHours.textContent = convertMs(ms).hours;
+//           timerMinutes.textContent = convertMs(ms).minutes;
+//           timerSeconds.textContent = convertMs(ms).seconds;
+
+//           if (ms < 1000) {
+//             clearInterval(timerId);
+//             timerDays.textContent = '00';
+//             timerHours.textContent = '00';
+//             timerMinutes.textContent = '00';
+//             timerSeconds.textContent = '00';
+//           }
+//         }, 1000);
+//       });
+//     }
+//   },
+// });
+
+// function convertMs(ms) {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+//   const days = Math.floor(ms / day);
+//   const hours = Math.floor((ms % day) / hour);
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+//   return { days, hours, minutes, seconds };
+// }
+
+// console.log(convertMs(2000));
+// console.log(convertMs(140000));
+// console.log(convertMs(24140000));
+
+
+
+
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from "notiflix";
 const input = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
-
 let timerDays = document.querySelector('span[data-days]');
 let timerHours = document.querySelector('span[data-hours]');
 let timerMinutes = document.querySelector('span[data-minutes]');
 let timerSeconds = document.querySelector('span[data-seconds]');
-
 const currentDate = new Date();
-
 startBtn.disabled = true;
 let timerId;
-
 const fp = flatpickr(input, {
   enableTime: true,
   time_24hr: true,
@@ -33,12 +101,11 @@ const fp = flatpickr(input, {
         const timerId = setInterval(() => {
           const currentTime = new Date();
           const ms = selectedDates[0].getTime() - currentTime.getTime();
-
-          timerDays.textContent = convertMs(ms).days;
-          timerHours.textContent = convertMs(ms).hours;
-          timerMinutes.textContent = convertMs(ms).minutes;
-          timerSeconds.textContent = convertMs(ms).seconds;
-
+          const formattedTime = convertMs(ms);
+          timerDays.textContent = formatTwoDigits(formattedTime.days);
+          timerHours.textContent = formatTwoDigits(formattedTime.hours);
+          timerMinutes.textContent = formatTwoDigits(formattedTime.minutes);
+          timerSeconds.textContent = formatTwoDigits(formattedTime.seconds);
           if (ms < 1000) {
             clearInterval(timerId);
             timerDays.textContent = '00';
@@ -51,7 +118,6 @@ const fp = flatpickr(input, {
     }
   },
 });
-
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
@@ -63,9 +129,9 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   return { days, hours, minutes, seconds };
 }
-
+function formatTwoDigits(value) {
+  return value.toString().padStart(2, '0');
+}
 console.log(convertMs(2000));
 console.log(convertMs(140000));
 console.log(convertMs(24140000));
-
-
